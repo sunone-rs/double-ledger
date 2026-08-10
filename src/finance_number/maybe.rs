b29f8bd60a7bfcad6619524f60872c337e75855b
sunone-rs <byte21c@gmail.com>
2026-08-10 09:20:58 +0900

    refactor: introduce explicit CalcErr and CurrencyUnit trait to improve error handling and type safety

--- src/finance_number/maybe.rs
+++ src/finance_number/maybe.rs
@@ -1,81 +1,375 @@
-use std::ops::{Add, Div, Mul, Rem, Sub};
+use std::ops::{Add, Div, Mul, Neg, Rem, Sub};
 
-use num::{CheckedAdd, CheckedDiv, CheckedMul, CheckedSub, traits::CheckedRem};
+use num::{
+    CheckedAdd, CheckedDiv, CheckedMul, CheckedSub,
+    traits::{CheckedNeg, CheckedRem},
+};
 
+#[derive(Clone, Copy, Debug, PartialEq, Eq)]
+pub enum CalcErr {
+    Overflow,
+    Underflow,
+    DivideByZero,
+    NoneValue,
+}
 
-/// 失敗するかもしれない計算を行うためのラップ型
 /// Wrapping type that provide a calculation may be fail.
-#[derive(Clone,Copy)]
-pub struct MaybeCalc<T>(Option<T>);
+///
+/// # Safety
+/// - panic not be occured before unwrap.
+/// - checked arithmetic used.
+///
+#[derive(Clone, Copy)]
+pub enum MaybeCalc<T> {
+    Just(T),
+    Error(CalcErr),
+}
 
-impl<T> MaybeCalc<T>{
-    pub fn just(from:T)->Self{
-        MaybeCalc(Some(from))
+impl<T> MaybeCalc<T> {
+    /// Create a MaybeCalc as a valid value.
+    pub fn just(from: T) -> Self {
+        MaybeCalc::Just(from)
     }
 
-    pub fn unwrap(self)->T{
-        self.0.unwrap()
+    /// Return the containd value and consuming self value.
+    /// # Panics
+    /// - When the MaybeCalc contains invalid value, panic is occured.
+    pub fn unwrap(self) -> T {
+        match self {
+            MaybeCalc::Just(x) => x,
+            MaybeCalc::Error(e) => panic!("Error value unwrapped {:?}", e),
+        }
+    }
+
+    /// Return the Result<T,CalcError> and consuming self value.
+    pub fn to_result(self) -> Result<T, CalcErr> {
+        match self {
+            MaybeCalc::Just(x) => Ok(x),
+            MaybeCalc::Error(e) => Err(e),
+        }
+    }
+
+    pub fn and_then<F, U>(self, f: F) -> MaybeCalc<U>
+    where
+        F: FnOnce(T) -> MaybeCalc<U>,
+    {
+        match self {
+            MaybeCalc::Just(x) => f(x),
+            MaybeCalc::Error(e) => MaybeCalc::Error(e),
+        }
+    }
+
+    pub fn or_else<F>(self, f: F) -> MaybeCalc<T>
+    where
+        F: FnOnce(CalcErr) -> MaybeCalc<T>,
+    {
+        match self {
+            MaybeCalc::Just(x) => MaybeCalc::Just(x),
+            MaybeCalc::Error(e) => f(e),
+        }
+    }
+
+    pub fn ok_or(self, err: CalcErr) -> Result<T, CalcErr> {
+        match self {
+            Self::Just(x) => Ok(x),
+            Self::Error(_) => Err(err),
+        }
     }
 }
 
-impl<T> From<Option<T>> for MaybeCalc<T>{
+impl<T> From<Option<T>> for MaybeCalc<T> {
     fn from(value: Option<T>) -> Self {
-        MaybeCalc(value)
+        match value {
+            Some(x) => MaybeCalc::Just(x),
+            None => MaybeCalc::Error(CalcErr::NoneValue),
+        }
     }
 }
 
-impl<T> From<T> for MaybeCalc<T>{
+impl<T> From<T> for MaybeCalc<T> {
     fn from(value: T) -> Self {
-        MaybeCalc(Some(value))
+        MaybeCalc::Just(value)
     }
 }
 
 impl<T> Add<Self> for MaybeCalc<T>
-where T:CheckedAdd {
+where
+    T: CheckedAdd,
+{
     type Output = Self;
 
     fn add(self, rhs: Self) -> Self::Output {
-        MaybeCalc(self.0.and_then(|x|rhs.0.and_then(|y|x.checked_add(&y))))
-    }
-} 
-
-impl<T> Sub<Self> for MaybeCalc<T>
-where T:CheckedSub {
-    type Output = Self;
-
-    fn sub(self, rhs: Self) -> Self::Output {
-        MaybeCalc(self.0.and_then(|x|rhs.0.and_then(|y|x.checked_sub(&y))))
+        self.and_then(|l| {
+            rhs.and_then(|r| match l.checked_add(&r) {
+                Some(x) => MaybeCalc::Just(x),
+                None => MaybeCalc::Error(CalcErr::Overflow),
+            })
+        })
     }
 }
 
+impl<T> Sub<Self> for MaybeCalc<T>
+where
+    T: CheckedSub,
+{
+    type Output = Self;
+    #[inline]
+    fn sub(self, rhs: Self) -> Self::Output {
+        self.and_then(|l| {
+            rhs.and_then(|r| match l.checked_sub(&r) {
+                Some(x) => MaybeCalc::Just(x),
+                None => MaybeCalc::Error(CalcErr::Overflow),
+            })
+        })
+    }
+}
 
 impl<T> Mul<Self> for MaybeCalc<T>
-where T:CheckedMul {
+where
+    T: CheckedMul,
+{
     type Output = Self;
-    
+    #[inline]
     fn mul(self, rhs: Self) -> Self::Output {
-        MaybeCalc(self.0.and_then(|x|rhs.0.and_then(|y|x.checked_mul(&y))))
+        self.and_then(|l| {
+            rhs.and_then(|r| match l.checked_mul(&r) {
+                Some(x) => MaybeCalc::Just(x),
+                None => MaybeCalc::Error(CalcErr::Overflow),
+            })
+        })
     }
 }
 
 impl<T> Div<Self> for MaybeCalc<T>
-where T:CheckedDiv {
+where
+    T: CheckedDiv + PartialEq<T> + num::Zero,
+{
     type Output = Self;
-
+    #[inline]
     fn div(self, rhs: Self) -> Self::Output {
-        MaybeCalc(self.0.and_then(|x|rhs.0.and_then(|y|x.checked_div(&y))))
+        self.and_then(|l| {
+            rhs.and_then(|r| {
+                if r == T::zero() {
+                    return MaybeCalc::Error(CalcErr::DivideByZero);
+                }
+                match l.checked_div(&r) {
+                    Some(x) => MaybeCalc::Just(x),
+                    None => MaybeCalc::Error(CalcErr::Overflow),
+                }
+            })
+        })
     }
 }
 
 impl<T> Rem<Self> for MaybeCalc<T>
-where T:CheckedRem {
+where
+    T: CheckedRem + PartialEq<T> + num::Zero,
+{
     type Output = Self;
-
-    fn rem(self, rhs:Self) -> Self::Output {
-        MaybeCalc(self.0.and_then(|x|rhs.0.and_then(|y|x.checked_rem(&y))))
+    #[inline]
+    fn rem(self, rhs: Self) -> Self::Output {
+        self.and_then(|l| {
+            rhs.and_then(|r| {
+                if r == T::zero() {
+                    return MaybeCalc::Error(CalcErr::DivideByZero);
+                }
+                match l.checked_rem(&r) {
+                    Some(x) => MaybeCalc::Just(x),
+                    None => MaybeCalc::Error(CalcErr::Overflow),
+                }
+            })
+        })
     }
 }
 
+impl<T> Neg for MaybeCalc<T>
+where
+    T: CheckedNeg,
+{
+    type Output = Self;
+    #[inline]
+    fn neg(self) -> Self::Output {
+        self.and_then(|x| match x.checked_neg() {
+            Some(x) => MaybeCalc::Just(x),
+            None => MaybeCalc::Error(CalcErr::Overflow),
+        })
+    }
+}
 
+#[cfg(test)]
+mod test {
+    use crate::finance_number::maybe::CalcErr;
 
+    use super::MaybeCalc;
 
+    #[test]
+    fn test_add() {
+        let a = MaybeCalc::just(1);
+        let b = MaybeCalc::just(2);
+        let c = a + b;
+        assert_eq!(c.unwrap(), 3);
+    }
+
+    #[test]
+    #[should_panic]
+    fn test_bad_add_panic() {
+        let a = MaybeCalc::just(i128::MAX);
+        let b = MaybeCalc::just(1);
+        let c = a + b;
+        c.unwrap();
+    }
+
+    #[test]
+    fn test_bad_add_return_overflow() {
+        let a = MaybeCalc::just(i128::MAX);
+        let b = MaybeCalc::just(1);
+        let c = a + b;
+        assert!(matches!(c, MaybeCalc::Error(CalcErr::Overflow)));
+    }
+
+    #[test]
+    fn test_sub() {
+        let a = MaybeCalc::just(1);
+        let b = MaybeCalc::just(2);
+        let c = a - b;
+        assert_eq!(c.unwrap(), -1);
+    }
+
+    #[test]
+    fn test_bad_sub_return_none() {
+        let a = MaybeCalc::just(i128::MIN);
+        let b = MaybeCalc::just(1);
+        let c = a - b;
+        assert!(matches!(c, MaybeCalc::Error(CalcErr::Overflow)));
+    }
+
+    #[test]
+    #[should_panic]
+    fn test_bad_sub_panic() {
+        let a = MaybeCalc::just(i128::MIN);
+        let b = MaybeCalc::just(1);
+        let c = a - b;
+        c.unwrap();
+    }
+
+    #[test]
+    fn test_mul() {
+        let a = MaybeCalc::just(1);
+        let b = MaybeCalc::just(2);
+        let c = a * b;
+        assert_eq!(c.unwrap(), 2);
+    }
+
+    #[test]
+    #[should_panic]
+    fn test_bad_mul_panic() {
+        let a = MaybeCalc::just(i128::MAX);
+        let b = MaybeCalc::just(2);
+        let c = a * b;
+        c.unwrap();
+    }
+
+    #[test]
+    fn test_bad_mul_return_none() {
+        let a = MaybeCalc::just(i128::MAX);
+        let b = MaybeCalc::just(2);
+        let c = a * b;
+        assert!(matches!(c, MaybeCalc::Error(CalcErr::Overflow)));
+    }
+
+    #[test]
+    fn test_div() {
+        let a = MaybeCalc::just(2);
+        let b = MaybeCalc::just(2);
+        let c = a / b;
+        assert_eq!(c.unwrap(), 1);
+    }
+
+    #[test]
+    fn test_div_undivisible() {
+        let a = MaybeCalc::just(1);
+        let b = MaybeCalc::just(2);
+        let c = a / b;
+        assert_eq!(c.unwrap(), 0);
+    }
+
+    #[test]
+    fn test_div_by_zero() {
+        let a = MaybeCalc::just(i128::MAX);
+        let b = MaybeCalc::just(0);
+        let c = a / b;
+        assert!(matches!(c, MaybeCalc::Error(CalcErr::DivideByZero)));
+    }
+
+    #[test]
+    #[should_panic]
+    fn test_bad_div_panic() {
+        let a = MaybeCalc::just(i128::MAX);
+        let b = MaybeCalc::just(0);
+        let c = a / b;
+        c.unwrap();
+    }
+
+    #[test]
+    fn test_rem() {
+        let a = MaybeCalc::just(1);
+        let b = MaybeCalc::just(2);
+        let c = a % b;
+        assert_eq!(c.unwrap(), 1);
+    }
+
+    #[test]
+    fn test_rem_by_zero() {
+        let a = MaybeCalc::just(i128::MAX);
+        let b = MaybeCalc::just(0);
+        let c = a % b;
+        assert!(matches!(c, MaybeCalc::Error(CalcErr::DivideByZero)));
+    }
+
+    #[test]
+    #[should_panic]
+    fn test_bad_rem_panic() {
+        let a = MaybeCalc::just(i128::MAX);
+        let b = MaybeCalc::just(0);
+        let c = a % b;
+        c.unwrap();
+    }
+
+    #[test]
+    fn test_div_min_neg_one() {
+        let a = MaybeCalc::just(i128::MIN);
+        let b = MaybeCalc::just(-1);
+        let c = a / b;
+        assert!(matches!(c, MaybeCalc::Error(CalcErr::Overflow)));
+    }
+
+    #[test]
+    fn test_adversarial_rem_min_neg_one() {
+        let a = MaybeCalc::just(i128::MIN);
+        let b = MaybeCalc::just(-1);
+        let c = a % b;
+        assert!(matches!(c, MaybeCalc::Error(CalcErr::Overflow)));
+    }
+
+    #[test]
+    fn test_adversarial_neg_min() {
+        let a = MaybeCalc::just(i128::MIN);
+        let c = -a;
+        assert!(matches!(c, MaybeCalc::Error(CalcErr::Overflow)));
+    }
+
+    #[test]
+    fn test_adversarial_none_propagation() {
+        let none = MaybeCalc::<i32>::from(None);
+        let some = MaybeCalc::just(10);
+
+        // None + Some = None
+        assert!(matches!(none + some, MaybeCalc::Error(CalcErr::NoneValue)));
+
+        // Some + None = None
+        assert!(matches!(some + none, MaybeCalc::Error(CalcErr::NoneValue)));
+
+        // None + None = None
+        assert!(matches!(none + none, MaybeCalc::Error(CalcErr::NoneValue)));
+    }
+}
--- src/money.rs
+++ src/money.rs
@@ -1,12 +1,55 @@
-use std::{
-    marker::PhantomData,
-    ops::{Add, Sub},
-};
+use std::{ops::Add, sync::Arc};
 
-use crate::finance_number::integer::FinanceInt;
-pub trait Currency {}
+use num::CheckedAdd;
 
 #[derive(Debug)]
-pub struct Money{
-    amount:FinanceInt,
+pub struct Money<Currency>
+where
+    Currency: CurrencyUnit,
+{
+    amount: u128,
+    rule: Arc<Currency>,
+}
+
+impl<T> Money<T> where T: CurrencyUnit {}
+
+impl<T> CheckedAdd for Money<T>
+where
+    T: CurrencyUnit,
+{
+    fn checked_add(&self, v: &Self) -> Option<Self> {
+        self.amount.checked_add(v.amount).map(|amount| Money {
+            amount,
+            rule: self.rule.clone(),
+        })
+    }
+}
+
+impl<T> Add for Money<T>
+where
+    T: CurrencyUnit,
+{
+    type Output = Self;
+    fn add(self, rhs: Self) -> Self::Output {
+        eprintln!(
+            "\x1b[31m Unchecked add is used. \x1b[0m Use checked_add or MaybeCalc instead of this method."
+        );
+        Money {
+            amount: self
+                .amount
+                .checked_add(rhs.amount)
+                .expect("The Amount overflow."), // Safe guard for finance stability. Will be handled by MaybeCalc.
+            rule: self.rule,
+        }
+    }
+}
+
+/// The trait about currency unit.
+/// # Summary
+/// This trait define properties and rules of currency.
+///
+pub trait CurrencyUnit {
+    fn currency_code() -> String;
+    fn currency_symbol() -> String;
+    fn currency_decimal_places() -> u32;
 }
